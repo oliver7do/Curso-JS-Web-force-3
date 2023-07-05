7b17b59fb2a948d743cb83def2690811d50c6328
oliver7do <afristreaming@gmail.com>
2023-07-05 16:55:17 +0200

    5 commit

---
+++ Nouveau dossier/mystere/mistere.js
@@ -0,0 +1,13 @@
+// creer une variable pour chaque bouton
+
+// creer une variable pour chaque input
+
+// ecouter le click sur le bouton solution
+// si on click sur le bouton solution il affiche le nombre mystere dans message
+
+
+
+// ecouter le click sur le bouton nouveau jeu
+// si on click dessus il genere un nombre aleatoire entre 0 et 50 puis remet le nombre d'essaies a zero
+
+// pour generer un nombre aleatoire utiliser l'objet math
